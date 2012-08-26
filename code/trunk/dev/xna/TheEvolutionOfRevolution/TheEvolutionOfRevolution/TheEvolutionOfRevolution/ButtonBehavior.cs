83f4a675def444cc6f4c27ffa5a6945128d5a955
williansilva.nave <williansilva.nave@gmail.com>
2012-08-26 11:52:20 +0000

    - Comportamento de botão adicionado

---
+++ code/trunk/dev/xna/TheEvolutionOfRevolution/TheEvolutionOfRevolution/TheEvolutionOfRevolution/ButtonBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace TheEvolutionOfRevolution
+{
+    class ButtonBehavior
+    {
+        MouseState currentState;
+        bool pressing = false;
+        public bool PRESSED = false;
+
+        public void CheckButton(Rectangle rectangle)
+        {
+            currentState = Mouse.GetState();
+
+            if (rectangle.Contains(new Point(currentState.X, currentState.Y)))
+            {
+                PRESSED = false;
+
+                if (currentState.LeftButton == ButtonState.Pressed)
+                {
+                    pressing = true;
+                }
+                else
+                {
+                    if (pressing) { PRESSED = true; }
+                    pressing = false;
+                }
+            }
+            else
+            {
+                pressing = false;
+                PRESSED = false;
+            }
+        }
+    }
+}
